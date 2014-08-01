596c51934825d96a4eb34bcf671103386e430a2c
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2014-08-01 23:10:15 +0100

    Added utilities file and folder, themes and gitignore.

---
+++ utilities.el
@@ -0,0 +1,9 @@
+(message "uti")
+
+(add-to-list 'load-path "~/.emacs.d/utilities")
+
+(require 'windata) ;required by dirtree
+(require 'tree-mode) ;required by dirtree
+(require 'dirtree)
+
+(require 'transpose-frame)
