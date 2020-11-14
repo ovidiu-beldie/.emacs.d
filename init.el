bf6ba8c07655be2ce91e2764bd15cff06ee764ac
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2020-11-14 21:56:59 +0000

    Enabled display of line and col nr

--- init.el
+++ init.el
@@ -22,8 +22,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(line-number-mode nil)
  '(package-selected-packages
-   '(which-key yasnippet avy counsel auto-package-update aggressive-indent aggresive-indent rainbow-mode diminish company ivy doom-modeline use-package org-bullets doom-themes all-the-icons)))
+   '(prog-mode rainbow-delimiters display-line-numbers-mode which-key yasnippet avy counsel auto-package-update aggressive-indent aggresive-indent rainbow-mode diminish company ivy doom-modeline use-package org-bullets doom-themes all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
